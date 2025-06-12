23d9838693f8b7c6348bfdc8b0bcf85402862f38
KasperTonsgaard <kasper.tonsgaard@merkle.com>
2025-06-12 12:37:06 +0200

    Add Type support for model-viewer

--- app/components/ocaml.tsx
+++ app/components/ocaml.tsx
@@ -5,13 +5,13 @@ export default function OCaml() {
     <>
       <Script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/4.0.0/model-viewer.min.js" />
       <model-viewer
-        class="ocaml-model"
+        className="ocaml-model"
         src="models/Camel3D.glb" 
         camera-controls tone-mapping="neutral" 
         shadow-intensity="1.01" auto-rotate exposure="1.5"
       >
-        <div class="progress-bar hide" slot="progress-bar">
-            <div class="update-bar"></div>
+        <div className="progress-bar hide" slot="progress-bar">
+            <div className="update-bar"></div>
         </div>
       </model-viewer>
     </>
