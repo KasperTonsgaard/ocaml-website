7d9443d2291bb137120bf0b856f1d56f19cea3af
KasperTonsgaard <kasper.tonsgaard@merkle.com>
2025-11-09 05:28:26 +0100

    fix type

--- model-viewer.d.ts
+++ model-viewer.d.ts
@@ -1,15 +1,20 @@
-import { ModelViewerElement } from "@google/model-viewer";
-import * as React from "react";
+import 'react';
 
-declare global {
+declare module 'react' {
   namespace JSX {
     interface IntrinsicElements {
-      "model-viewer": React.DetailedHTMLProps<
-        React.HTMLAttributes<ModelViewerElement> & Partial<ModelViewerElement>,
-        ModelViewerElement
-      >;
+      'model-viewer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        src?: string;
+        alt?: string;
+        'camera-controls'?: boolean;
+        'disable-pan'?: boolean;
+        'tone-mapping'?: string;
+        'shadow-intensity'?: string;
+        'auto-rotate'?: boolean;
+        exposure?: string;
+        class?: string;
+        ref?: any;
+      };
     }
   }
 }
-
-export {};
