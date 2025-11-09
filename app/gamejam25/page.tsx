0fb14e646bceb85c0068939f9fad5945c74aecdf
Mikkel Maae Østergaard <mikkelmaae@gmail.com>
2025-11-09 02:50:30 +0100

    Add gamejam 25 game page

---
+++ app/gamejam25/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function Page() {
+
+    return (
+        <section>
+            <iframe src="https://gamejam25.omikkel.com" frameborder="0"></iframe>
+        </section>
+    )
+}
