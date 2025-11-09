9f52d241d15e03dd04bfb7228aae439bb2ef0f1c
Mikkel Maae Østergaard <mikkelmaae@gmail.com>
2025-11-09 02:50:21 +0100

    Add header

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,40 +1,23 @@
 import Link from 'next/link'
 
-const navItems = {
-  '/': {
-    name: 'home',
-  },
-  '/blog': {
-    name: 'blog',
-  },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy',
-  },
-}
-
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
-      <div className="lg:sticky lg:top-20">
-        <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-          id="nav"
-        >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
-                >
-                  {name}
-                </Link>
-              )
-            })}
-          </div>
-        </nav>
-      </div>
-    </aside>
+    <header className="w-full p-4 flex justify-center items-center border-b border-gray-300 dark:border-gray-700">
+        <div>
+            <h1>OCaml's Universe</h1>
+        </div>
+        <div className='flex flex-grow justify-center items-center'>
+            <nav>
+                <ul className="flex space-x-4">
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/gamejam25">Game Jam 25</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,7 @@ export default function RootLayout({
       )}
     >
       <body className="">
+        <Navbar />
         <main className="">
           {children}
         </main>
